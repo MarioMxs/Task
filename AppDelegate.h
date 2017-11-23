93e0716472825a258c782420e332cde9745e8723
maxiaosong <xiaosong.ma@pingcl.com>
2017-11-23 11:28:51 +0800

    测试

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  task
+//
+//  Created by Mario on 2017/11/23.
+//  Copyright © 2017年 Mario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
